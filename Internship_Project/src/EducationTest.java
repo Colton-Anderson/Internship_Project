6dc870b005769ceb5c33f850af33b7d4e9f44680
Colton-Anderson <coltonanderson10@gmail.com>
2021-11-14 23:25:19 -0500

    commit

--- Internship_Project/src/EducationTest.java
+++ Internship_Project/src/EducationTest.java
@@ -8,5 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EducationTest {
-    
+    private Education education = new Education("School",2023,"major","minor",3.8);
+
+    @Test
+    public void testToString() {
+        String outcome = education.getSchool() + "\nClass Year: " + education.getClassYear() + "\nMajor: " + education.getMajor() +
+        "\nMinor: " + education.getMinor() + "\nGPA: " + education.getGpa();
+
+        assertEquals(outcome, education.toString());
+    }
 }
